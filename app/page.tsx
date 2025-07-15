250428e99fd90e58dbb92f1f1ce37c3fed253923
mcnemy001 <mclarenamaleta001@gmail.com>
2025-07-15 19:58:48 +0700

    Update Loading Animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { GlowingEffectDemoSecond } from '@/components/BentoGlowing';
 import { RecentProject } from '@/components/RecentProject';
 import { FloatingNav } from '@/components/ui/FloatingNav';
@@ -7,8 +9,33 @@ import { Footer } from '@/components/Footer';
 import { AboutMe } from '@/components/AboutMe';
 import { Tools } from '@/components/Tools';
 import { TimelineDemo } from '@/components/TimelineWork';
+import { useEffect, useState } from 'react';
+
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const LoadingScreen = () => {
+    return (
+      <div className="flex justify-center items-center h-screen w-screen bg-black-100">
+        <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-cyan-500"></div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    // Simulasi atau proses fetch data
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
@@ -17,7 +44,7 @@ export default function Home() {
           <LampDemo />
         </div>
 
-        <AboutMe/>
+        <AboutMe />
         <GlowingEffectDemoSecond />
         <RecentProject />
         <Tools />

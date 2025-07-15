3476912d2334db361c8d2bcba70dff092dffdc70
mcnemy001 <mclarenamaleta001@gmail.com>
2025-07-15 18:13:35 +0700

    Chane Navigation

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: 'About', link: '#about' },
   { name: 'Projects', link: '#projects' },
-  { name: 'Work Experience', link: '#timeline' },
+  { name: 'Experience', link: '#timeline' },
   { name: 'Contact', link: '#contact' },
 ];
 

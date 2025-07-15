25f855d647e0f37e10677b2cb9c88a758fa37267
mcnemy001 <mclarenamaleta001@gmail.com>
2025-07-15 19:18:57 +0700

    Update Loading Animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,11 @@
 export default function Loading() {
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-black-100">
-      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-cyan-500"></div>
+      <div className="flex space-x-2">
+        <div className="w-4 h-4 rounded-full bg-cyan-500 animate-bounce"></div>
+        <div className="w-4 h-4 rounded-full bg-cyan-500 animate-bounce delay-150"></div>
+        <div className="w-4 h-4 rounded-full bg-cyan-500 animate-bounce delay-300"></div>
+      </div>
     </div>
   );
 }
